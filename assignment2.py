16853e19b316be4c9adef88d57a542056321b5b6
msroudsari <ms.roudsari@gmail.com>
2025-03-18 11:53:55 -0500

    02

--- assignment2.py
+++ assignment2.py
@@ -1,20 +1,16 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
-
-
 train_data = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv")
 test_data = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3test.csv")
 
-X_train = train_data.drop(columns=['meal'])
-y_train = train_data['meal']
+X_train = train_data.drop("meal", axis=1)
+y_train = train_data["meal"]
+X_test = test_data.drop("meal", axis=1)
 
-X_test = test_data
+model = RandomForestClassifier(n_estimators=200, max_depth=8, random_state=42)
 
-model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=42)
 modelFit = model.fit(X_train, y_train)
-pred = modelFit.predict(X_test)
-pred = pred.astype(int)
 
-print(pred)
-print(f"Total predictions: {len(pred)}")
+pred = modelFit.predict(X_test)
+pred = [int(i) for i in pred]
