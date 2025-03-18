aa80e044843c27693dd9eebd8ec69e2e08fadafb
msroudsari <ms.roudsari@gmail.com>
2025-03-18 12:03:23 -0500

    03

--- assignment2.py
+++ assignment2.py
@@ -4,13 +4,16 @@ from sklearn.ensemble import RandomForestClassifier
 train_data = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv")
 test_data = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3test.csv")
 
-X_train = train_data.drop("meal", axis=1)
+X_train = train_data.drop(columns=['id','DateTime','meal'])
+
 y_train = train_data["meal"]
-X_test = test_data.drop("meal", axis=1)
+
+X_test = test_data.drop(columns=['id','DateTime','meal'])
 
 model = RandomForestClassifier(n_estimators=200, max_depth=8, random_state=42)
 
 modelFit = model.fit(X_train, y_train)
 
 pred = modelFit.predict(X_test)
-pred = [int(i) for i in pred]
+pred = pred.astype(int)
+pred = pred.tolist()
